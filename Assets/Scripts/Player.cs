87cfe625f07c387e9cde2928e22ac9b464bdd253
LiadOvdat5 <LiadOvdat5@gmail.com>
2021-08-11 16:45:03 +0300

    2nd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Camera mainCamera;
+    bool isCatching;
 
     // Start is called before the first frame update
     void Start()
@@ -15,21 +16,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPos.z = 0;
-        transform.position = mouseWorldPos;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition)), Vector2.zero, 0);
+            if (hit)
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    isCatching = true;
+                }
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isCatching = false;
+        }
+        if (isCatching && Input.GetMouseButton(0))
+        {
+            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0;
+            transform.position = mouseWorldPos;   
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Obstacle")
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            Destroy(gameObject);
-        }
+            if (collision.gameObject.tag == "Obstacle")
+            {
+                Destroy(gameObject);
+            }
 
-        if(collision.gameObject.tag == "Finish Target")
-        {
-            Debug.Log("you win ! ");
+            if (collision.gameObject.tag == "Finish Target")
+            {
+                Debug.Log("you win ! ");
+            }
         }
-    }
-}
+    }   
