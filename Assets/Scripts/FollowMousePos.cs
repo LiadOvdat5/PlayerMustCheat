80356d0ac7e1d9559514e21a08f52ee9d29b04b9
LiadOvdat5 <LiadOvdat5@gmail.com>
2021-08-15 13:33:14 +0300

    more lights

---
+++ Assets/Scripts/FollowMousePos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMousePos : MonoBehaviour
+{
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        FollowMousePosition();
+    }
+
+    void FollowMousePosition()
+    {
+        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0;
+        transform.position = mouseWorldPos;
+    }
+
+}
